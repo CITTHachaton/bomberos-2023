b79bdfe7576e96f6ff5e6260114f3a303084f19e
bemtorres <114621426+profe-benja@users.noreply.github.com>
2023-06-10 00:03:52 -0400

    update

--- resources/views/mapa.blade.php
+++ resources/views/mapa.blade.php
@@ -9,11 +9,30 @@
 
 @endpush
 @section('content')
+<div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+  <h1 class="h2">Mapa de focos</h1>
+  <div class="btn-toolbar mb-2 mb-md-0">
+      <div class="btn-group me-2">
+        <button type="button" class="btn btn-sm btn-outline-secondary">Share</button>
+        <button type="button" class="btn btn-sm btn-outline-secondary">Export</button>
+      </div>
+      <button type="button" class="btn btn-sm btn-outline-secondary dropdown-toggle d-flex align-items-center gap-1">
+        <svg class="bi">
+            <use xlink:href="#calendar3" />
+        </svg>
+        This week
+      </button>
+  </div>
+</div>
 
 <div class="container">
+
   <div class="row">
+
+
+    <h2>Section title</h2>
     <div class="mt-3">
-      <div id="map"></div>
+      <div id="map" class="shadow"></div>
     </div>
   </div>
 </div>
