a015a6e9c59841ce5c79135f4be46a0b5fa94293
CarlosOrellana00 <carlosorellana.oa@gmail.com>
2023-06-10 03:37:13 -0400

    img bombero

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -191,15 +191,11 @@
   <div class="container col-xxl-8 px-4 py-5">
     <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
       <div class="col-10 col-sm-8 col-lg-6">
-        <img src="bootstrap-themes.png" class="d-block mx-lg-auto img-fluid" alt="Bootstrap Themes" width="700" height="500" loading="lazy">
+        <img src="{{ asset('img/bomberoslogo.png') }}" class="d-block mx-lg-auto img-fluid" alt="Bootstrap Themes" width="700" height="500" loading="lazy">
       </div>
       <div class="col-lg-6">
-        <h1 class="display-5 fw-bold text-body-emphasis lh-1 mb-3">Responsive left-aligned hero with image</h1>
-        <p class="lead">Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system, extensive prebuilt components, and powerful JavaScript plugins.</p>
-        <div class="d-grid gap-2 d-md-flex justify-content-md-start">
-          <button type="button" class="btn btn-primary btn-lg px-4 me-md-2">Primary</button>
-          <button type="button" class="btn btn-outline-secondary btn-lg px-4">Default</button>
-        </div>
+        <h1 class="display-5 fw-bold text-body-emphasis lh-1 mb-3">Nuestra necesidad a resolver</h1>
+        <p class="lead">En Maipú existen 2295 grifos disponibles para el uso de la 3ra compañía de bomberos de la comuna. De estos grifos se desconoce el estado,  sea operativo, dañado en desuso o simplemente ya no existen. Este desconocimiento entorpece la eficiencia bomberil para combatir los siniestros.</p>
       </div>
     </div>
   </div>
